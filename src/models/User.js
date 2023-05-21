d22416a0945b018e8a2e8471cebac94df9ee5417
Amanda Zanata <amandazanata46@gmail.com>
2023-05-21 19:39:55 -0300

    update:Configura migrations e Models de acordo com as regras do db

--- src/models/User.js
+++ src/models/User.js
@@ -1,27 +1,26 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class User extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
-  };
-  User.init({
-    id: DataTypes.INTEGER,
-    display_name: DataTypes.STRING,
+  const User = sequelize.define({
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    displayName: DataTypes.STRING,
     email: DataTypes.STRING,
     password: DataTypes.STRING,
-    image: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'User',
+    image: DataTypes.STRING,
+  },
+    {
+      timestamps: false,
+      underscored: true,
+      tableName: 'users',
   });
+
+  User.associate = (models) => {
+    User.hasMany(models.BlogPost, {
+      foreignKey: 'userId', as: 'blogPosts'
+    });
+  }
+
   return User;
 };
